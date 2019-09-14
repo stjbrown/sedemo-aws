4ac5c86399c2f960d18f7bc4d8e1d1695784a3d4
Stephen Brown <stjbrown@gmail.com>
2019-09-14 11:56:57 -0400

    Updated Script

--- getAWSCreds.sh
+++ getAWSCreds.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-accessKeyId=$(ssh -i ~/.ssh/gse2019-keypair.pem ec2-user@$1 "curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/NetskopeS3AdminRole/ -w '\n'" | jq -r '.AccessKeyId')
-secretAccessKey=$(ssh -i ~/.ssh/gse2019-keypair.pem ec2-user@$1 "curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/NetskopeS3AdminRole/ -w '\n'" | jq -r '.SecretAccessKey')
-sessionToken=$(ssh -i ~/.ssh/gse2019-keypair.pem ec2-user@$1 "curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/NetskopeS3AdminRole/ -w '\n'" | jq -r '.Token')
+accessKeyId=$(ssh -i $1 ec2-user@$2 "curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/NetskopeS3AdminRole/ -w '\n'" | jq -r '.AccessKeyId')
+secretAccessKey=$(ssh -i $1 ec2-user@$2 "curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/NetskopeS3AdminRole/ -w '\n'" | jq -r '.SecretAccessKey')
+sessionToken=$(ssh -i $1 ec2-user@$2 "curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/NetskopeS3AdminRole/ -w '\n'" | jq -r '.Token')
 
-export AWS_ACCESS_KEY_ID=$accessKeyId 
+export AWS_ACCESS_KEY_ID=$accessKeyId
 export AWS_SECRET_ACCESS_KEY=$secretAccessKey
 export AWS_SESSION_TOKEN=$sessionToken
 
